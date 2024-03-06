a18db40fd1e618a840b94bd1bcb729353d698c09
anton <a.tashchilin@bk.ru>
2024-03-06 13:50:02 +0400

    add change server

--- server/app.js
+++ server/app.js
@@ -7,21 +7,38 @@ const HOST = "localhost";
 const ERRS = [500,502,503,504,508]
 let errorOrSuccess = Math.random();
 
-
+const halding = (res,s="Success req")=>{
+    if (errorOrSuccess < 0.5) {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({msg:s}));
+        errorOrSuccess = Math.random();
+    } else {
+        res.statusCode = ERRS[Math.floor(Math.random()*(ERRS.length))];
+        res.setHeader("Content-Type", "application/json");
+        res.end(`{"message":"Error with status code ${res.statusCode}"}`);
+        errorOrSuccess = Math.random();
+    }
+}
 const server = http.createServer((req, res) => {
     if (req.url == ENDPOINT) {    
-        if (errorOrSuccess < 0.5) {
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.end(`{"message": "Success req"}`);
-            errorOrSuccess = Math.random();
-        } else {
-            res.statusCode = ERRS[Math.floor(Math.random()*(ERRS.length))];
-            res.setHeader("Content-Type", "application/json");
-            res.end(`{"message":"Error with status code ${res.statusCode}"}`);
-            errorOrSuccess = Math.random();
+        if (req.method=="GET") {
+            halding(res);
         }
-        
+        else {
+            let resBody = [];
+            req.on("data",(chunk) =>{
+                resBody.push(chunk);
+            });
+            req.on("end",()=>{
+                try {
+                    resBody = JSON.parse(Buffer.concat(resBody).toString());
+                } catch(e) {
+                    console.log(e);
+                }
+                halding(res,resBody);
+            });
+        }
     }
   
 });
